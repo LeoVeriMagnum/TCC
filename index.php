eaa9bbf1640e8c86c0f9a1d0b35b553cb26fdcfe
LeoVeriMagnum <leonardoverissimo1223@proton.me>
2023-03-18 18:03:00 -0300

    batata

--- index.php
+++ index.php
@@ -5,11 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <link rel="stylesheet" type="text/css" href="css/estilo_index.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <header></header>
+    <header id="header" class="p-2"> 
+        <nav class="nav-fill">
+            <div class="d-flex justify-content-between"> 
+                <div class="set1 img2">
+                    <img src="img/index/PawSome Home.png" width="100%" height="90%"></img>
+                </div>
+                <div class="set1 img1">
+                    <img src="img/index/2.png" alt="" width="80" height="80"></img>
+                </div>
+                <div class="set1">
+                    <ul class="nav justify-content-end">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Quem somos?</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cadastre-se</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Entrar</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+
+    </header>
     <div class="container"></div>
     <footer></footer>
 </body>
